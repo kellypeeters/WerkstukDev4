81dd2b77da0a70e06a9aabd61960a7d9d37f9298
kellypeeters <43609729+kellypeeters@users.noreply.github.com>
2020-04-28 13:56:38 +0200

    Filters toegevoegd voor video's en geanimeerd

---
+++ script.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    //Eenvoudige manier om de style van de filters van de video's te veranderen
+    $('.doelgroepbutton').on('click',function(){
+        $(this).toggleClass('doelgroepbutton-active');
+    }); 
+});
