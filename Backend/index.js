3a5bd7685fe074913f9bb688fcac09e9142fa0e6
kellypeeters <43609729+kellypeeters@users.noreply.github.com>
2020-05-14 14:26:21 +0200

    nodeserver code aangepast

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,34 @@
+"use strict";
+
 const express = require('express');
 const fetch = require('node-fetch');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 8000;
+let cors = require('cors');
+let bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(express.static('Frontend'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//Localhost url
+app.get('/', function (req, res) {
+    res.sendFile('/Frontend/index.html', {
+        root: './..' 
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
